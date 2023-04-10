b8bb6c6e52d6bebfb3a8aa22a85b4f42c4fa796a
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-10 10:31:29 +0530

    Update and rename Arithmetic Operators.c to B 101 : The If-Else Statement.c

---
+++ B 101 : The If-Else Statement.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1, var2;
+    scanf("%d %d",&var1,&var2);
+	(var1 >= var2)?printf("Value1 is not less than Value2 "):printf("Value1 is less than Value2");
+	return 0;
+}
