a19daa15b91a199cbd4080d393856c880351a455
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-11 10:50:58 +0530

    Create B 108 : Prime or Not.c

---
+++ B 108 : Prime or Not.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int number, is_prime=0;
+    int i=1;
+	scanf("%d", &number);
+	//your code here
+	while(i<number){
+        if(number%i==0){
+            is_prime++;
+        }
+    i++;
+    }
+    
+
+	if(number ==1) printf("no");
+	if (is_prime == 1) printf("yes");
+	if (is_prime > 1) printf("no");
+	return 0;
+}
