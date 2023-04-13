0de68dd72200e26c7cf56e5df7b3a93901705ed4
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-14 02:39:05 +0530

    Create B 116 : The Switch-Case Statement.c

---
+++ B 116 : The Switch-Case Statement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	char ch;
+	scanf("%c", &ch);
+	switch(ch)
+	{
+		case 'a' : printf("vowel"); break;
+		case 'e' : printf("vowel"); break;
+        case 'i' : printf("vowel"); break;
+        case 'o' : printf("vowel"); break;
+        case 'u' : printf("vowel"); break;
+    
+		
+	
+
+		default : printf("not vowel"); break;
+	}	
+	return 0;
+}
