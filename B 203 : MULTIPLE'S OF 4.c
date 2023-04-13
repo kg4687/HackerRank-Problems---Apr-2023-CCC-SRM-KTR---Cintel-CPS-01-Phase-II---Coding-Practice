f35230539e67479e081071a29a1c3413694d7a44
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-14 02:41:13 +0530

    Create B 203 : MULTIPLE'S OF 4.c

---
+++ B 203 : MULTIPLE'S OF 4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    (n%4==0)?printf("yes"):printf("no");
+    return 0;
+}
