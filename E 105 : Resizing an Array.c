2fc16a1f280b8c0de98ae35a1179c81b631be0b4
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-25 00:31:08 +0530

    Create E 105 : Resizing an Array.c

---
+++ E 105 : Resizing an Array.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int* arr = (int*) malloc(sizeof(int) * 3);
+    int i, N;
+    //input N and then use realloc to change size of array arr.
+    scanf("%d",&N);
+    realloc(arr,sizeof(int)*N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    for(i=N-1;i>=0;i--)
+    {
+        printf("%d ",*(arr+i));
+    }
+    
+    return 0;
+}
