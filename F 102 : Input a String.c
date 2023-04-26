71c7baf2403703b254fab942f29dfd803494ac0d
Kabir Gupta <104321998+kg4687@users.noreply.github.com>
2023-04-26 23:13:10 +0530

    Create F 102 : Input a String.c

---
+++ F 102 : Input a String.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[50];
+	//input string here using scanf
+scanf("%s",&str);
+	//print string here using printf
+	printf("%s",str);
+	return 0;
+}
